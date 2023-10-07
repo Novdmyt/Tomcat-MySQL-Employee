f9455fb0f9fe6ac05327c1017450358aef0f5fc7
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-07 12:11:17 +0300

    Create connecting DB

---
+++ src/main/java/org/example/app/database/DBConn.java
@@ -0,0 +1,30 @@
+package org.example.app.database;
+
+import org.example.app.utils.Constants;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConn {
+    private static final Logger LOGGER =
+            Logger.getLogger(DBConn.class.getName());
+
+    public static Connection connection() throws SQLException{
+        Connection connection = null;
+        try {
+            Class.forName(Constants.DB_DRIVER);
+                    }catch (ClassNotFoundException e){
+            throw new SQLException(e);
+                    }
+        try {
+            connection = DriverManager.getConnection(Constants.DB_URL + Constants.DB_NAME,
+                    Constants.DB_USER, Constants.DB_PASS);
+                    }catch (SQLException e){
+            LOGGER.log(Level.WARNING, e.getMessage(), e);
+        }
+        return connection;
+    }
+}
