7fa9fa123e3514e7039872d7083d876e12334432
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-09 17:47:35 +0300

    Add code in class Constants

--- src/main/java/org/example/app/utils/Constants.java
+++ src/main/java/org/example/app/utils/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public final static String DB_NAME = "demo_db";
     public final static String DB_USER = "dev";
     public final static String DB_PASS = Credentials.DB_PASS;
+    public final static String PHONE_RGX = "\\+\\d{2}\\(\\d{3}\\)\\d{3}\\d{2}\\d{2}";
 }
