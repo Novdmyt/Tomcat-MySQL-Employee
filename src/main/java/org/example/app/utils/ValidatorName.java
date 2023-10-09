6901821d2eff3d7b7a05f72d059866f7669c1079
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-09 17:46:14 +0300

    Create class ValidatorName

---
+++ src/main/java/org/example/app/utils/ValidatorName.java
@@ -0,0 +1,11 @@
+package org.example.app.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidatorName {
+    public static boolean isValidName(String name) {
+        return !name.isEmpty();
+    }
+
+}
